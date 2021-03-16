f2c80fd2e7380f7d5d2c7493a7880376b8c6be5f
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2021-03-16 08:17:59 +0000

    More tests

--- test_ticket_map.cpp
+++ test_ticket_map.cpp
@@ -1,6 +1,7 @@
 #include "ticket_map.hpp"
 #include <assert.h>
 #include <type_traits>
+#include <string>
 
 void test_initially_empty() {
     jss::ticket_map<int, int> map;
@@ -72,9 +73,32 @@ void test_can_iterate_over_values() {
     }
 }
 
+void test_empty_map_has_begin_equal_end() {
+    jss::ticket_map<int, std::string> map;
+    assert(map.begin() == map.end());
+}
+
+void test_begin_and_end_types() {
+    jss::ticket_map<int, std::string> map;
+
+    static_assert(
+        std::is_same<
+            typename decltype(map)::iterator, decltype(map.begin())>::value,
+        "begin must return an iterator");
+    static_assert(
+        std::is_same<decltype((*map.begin()).ticket), int const &>::value,
+        "key is specified type");
+    static_assert(
+        std::is_same<decltype((*map.begin()).value), std::string &>::value,
+        "value is ref");
+}
+
 int main() {
     test_initially_empty();
     test_inserting_a_value_gives_iterator_to_new_element();
     test_inserting_a_second_value_gives_new_ticket();
     test_inserting_a_bunch_of_elements_gives_iterators_and_updates_size();
+    test_can_iterate_over_values();
+    test_empty_map_has_begin_equal_end();
+    test_begin_and_end_types();
 }
